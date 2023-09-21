8fea59bddc83172bc6b26519223d21a58d6470e3
adamnmartinez <adam.nmartinez@gmail.com>
2023-09-21 12:57:37 -0700

    Implemented library search bar

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -5,46 +5,60 @@ export default function BookList(props: {
   editFunc: Function;
   notesFunc: Function;
   list: Book[];
+  query: string;
 }): React.ReactElement {
   const listElements: React.ReactElement[] = [];
 
+  const noResults = <li className="noresults">No Results</li>;
+
   props.list.forEach((book) => {
-    listElements.push(
-      <li>
-        <div className="bookWrapper">
-          <span className="bookName">
-            {book.title}
-            {book.publishedYear ? ` (${book.publishedYear})` : ""}{" "}
-          </span>
-          <hr />
-          by {book.author ? book.author : "Unknown"} <br />
-          {book.pages ? `${book.pages} pages ` : ""} {book.pages ? <br /> : ""}
-          {book.edition ? `Edition ${book.edition} ` : ""}{" "}
-          {book.edition ? <br /> : ""}
-          <div className="bookButtons">
-            <button
-              className="bookOperations"
-              onClick={() => props.editFunc(book)}
-            >
-              Edit
-            </button>
-            <button
-              className="bookOperations"
-              onClick={() => props.notesFunc(book)}
-            >
-              Notes
-            </button>
-            <button
-              className="bookOperations deleteBtn"
-              onClick={() => props.deleteFunc(book)}
-            >
-              Delete
-            </button>
+    if (
+      book.title.toLowerCase().includes(props.query.toLowerCase()) ||
+      book.author.toLowerCase().includes(props.query.toLowerCase()) ||
+      props.query === ""
+    ) {
+      listElements.push(
+        <li>
+          <div className="bookWrapper">
+            <span className="bookName">
+              {book.title}
+              {book.publishedYear ? ` (${book.publishedYear})` : ""}{" "}
+            </span>
+            <hr />
+            by {book.author ? book.author : "Unknown"} <br />
+            {book.pages ? `${book.pages} pages ` : ""}{" "}
+            {book.pages ? <br /> : ""}
+            {book.edition ? `Edition ${book.edition} ` : ""}{" "}
+            {book.edition ? <br /> : ""}
+            <div className="bookButtons">
+              <button
+                className="bookOperations"
+                onClick={() => props.editFunc(book)}
+              >
+                Edit
+              </button>
+              <button
+                className="bookOperations"
+                onClick={() => props.notesFunc(book)}
+              >
+                Notes
+              </button>
+              <button
+                className="bookOperations deleteBtn"
+                onClick={() => props.deleteFunc(book)}
+              >
+                Delete
+              </button>
+            </div>
           </div>
-        </div>
-      </li>,
-    );
+        </li>,
+      );
+    }
   });
 
-  return <ul className="bookList">{listElements}</ul>;
+  return (
+    <ul className="bookList">
+      {listElements.length !== 0 ? listElements : noResults}
+    </ul>
+  );
 }
--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -1,5 +1,6 @@
 import BookList from "../components/BookList";
 import { Book } from "../components/App";
+import { ChangeEvent, useState } from "react";
 
 export default function LibraryPage(props: {
   pullFunc: Function;
@@ -8,17 +9,35 @@ export default function LibraryPage(props: {
   editBook: Function;
   bookNotes: Function;
 }) {
+  const [libQuery, setLibQuery] = useState<string>("");
+
+  function handleSearch(event: ChangeEvent): void {
+    const target = event.currentTarget as HTMLInputElement;
+    setLibQuery(target.value);
+  }
+
   return (
     <div className="libraryPage">
-      <header>Compendium</header> <br />
+      <header>Compendium</header> Created by{" "}
+      <a href="http://www.adamnmartinez.com" target="_blank">
+        Adam Martinez
+      </a>{" "}
+      <br />
+      <br />
       <button className="addBookBtn" onClick={() => props.addBook()}>
         New Entry +
       </button>
+      <input
+        className="librarysearch"
+        placeholder="Search My Compendium"
+        onChange={handleSearch}
+      ></input>
       <BookList
         deleteFunc={props.pullFunc}
         editFunc={props.editBook}
         notesFunc={props.bookNotes}
         list={props.lib}
+        query={libQuery}
       />
     </div>
   );
